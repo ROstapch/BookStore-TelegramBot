f8f3dbd6a70f096f1a9d48c74600ec654f51381e
mark <mark@work.com>
2019-12-03 23:58:48 +0200

    testing library

--- TelegramBot/main.py
+++ TelegramBot/main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 from aiohttp import web
 import ssl
 
@@ -10,7 +11,14 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, 'Bot started, type /help to get additional help.')
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+	itembtn1 = types.KeyboardButton('Books')
+	itembtn2 = types.KeyboardButton('Authors')
+	itembtn3 = types.KeyboardButton('Publishers')
+	itembtn4 = types.KeyboardButton('test more')
+	markup.add(itembtn1)
+	bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
+	#bot.reply_to(message, 'Bot started, type /help to get additional help.')
 
-bot.polling()
+bot.polling(none_stop = True)
 
