c418125f7e30cb284a2f4795bc534e64155e150c
mark <mark@work.com>
2019-12-11 22:40:06 +0200

    still doing parsing

--- TelegramBot/bookstore_api.py
+++ TelegramBot/bookstore_api.py
@@ -1,64 +1,81 @@
 import requests
+import re
 
 
 class endpoints:
 	__home__ = "http://192.168.10.182:8000/"
+	books_page = __home__ + "api/books/?page="
+	authors_page = __home__ + "api/authors/?page="
+	publishers_page = __home__ + "api/publishers/?page="
 
 	class get:
 		def books(page=1):
-			url = endpoints.__home__ + "api/books/?page=" + str(page)
-			resp = requests.get(url=url, params="{'format':'api'}")
+			url = endpoints.books_page + str(page)
+			resp = requests.get(url=url)
 			return (resp)
 		
 		def authors(page=1):
-			url = endpoints.__home__ + "api/authors/?page=" + str(page)
-			resp = requests.get(url=url, params="{'format':'api'}")
+			url = endpoints.authors_page + str(page)
+			resp = requests.get(url=url)
 			return (resp)
 
 		def publishers(page=1):
-			url = endpoints.__home__ + "api/publishers/?page=" + str(page)
-			resp = requests.get(url=url, params="{'format':'api'}")
+			url = endpoints.publishers_page + str(page)
+			resp = requests.get(url=url)
 			return (resp)
 
 
 		def book_id(id=None):
 			url = endpoints.__home__ + "api/books/" + str(page)
-			resp = requests.get(url=url, params="{'format':'api'}")
+			resp = requests.get(url=url)
 			return (resp)
 		
 		def author_id(id=None):
 			url = endpoints.__home__ + "api/authors/" + str(page)
-			resp = requests.get(url=url, params="{'format':'api'}")
+			resp = requests.get(url=url)
 			return (resp)
 			
 		def publisher_id(id=None):
 			url = endpoints.__home__ + "api/publishers/" + str(page)
-			resp = requests.get(url=url, params="{'format':'api'}")
+			resp = requests.get(url=url)
 			return (resp)
 
 
 
 class response_parsed:
 	def __init__(self, response = None):
-		self.response = response
+		self.response = response if response else None
+		self.reply = "No books available right now"
+		self.ok = True if self.response else False
+		self.query_type = None
 
 	def page_header(func):
 		def wrapper(self, *args, **kwargs):
-			if (self.response):
-				self.total_pages = self.response.json().get('count')
-				self.next_page = self.response.json().get('next')
-				self.prev_page = self.response.json().get('previous')
+			if (self.ok):
+				self.total = self.response.json().get('count')
+				self.next_page = int(re.search(r'=\d+', self.response.json().get('next')).group()[1:]) if self.response.json().get('next') else None
+				self.prev_page = None
+				if (self.response.json().get('previous')):
+					if (re.search(r'=\d+', self.response.json().get('previous'))):
+						self.prev_page = int(re.search(r'=\d+', self.response.json().get('previous')).group()[1:])
+					else:
+						self.prev_page = 1
+				self.current_page = self.prev_page + 1 if self.prev_page else 1
 				self.data = self.response.json().get('results')
 				func(self, *args, **kwargs)
 				return(self)
+			return(self)
 		return (wrapper)
 
 	@page_header
 	def books(self):
-		if (self.response and self.data):
-			self.reply = self.data
-		else:
-			self.reply = "No books available right now"
+		if (self.ok and self.data):
+			self.query_type = "Books"
+			self.on_page = len(self.data)
+			self.reply = self.query_type + " on page %s:\n\n" % str(self.current_page)
+			for index, book in enumerate(self.data):
+				self.reply = self.reply + str(index + 1) + ". " + book.get("name") + " (" \
+					+ "published in " + str(book.get("year")) + ")\n"
 
 	@page_header
 	def authors(self):
--- TelegramBot/main.py
+++ TelegramBot/main.py
@@ -8,7 +8,6 @@ from bot_config import *
 from bookstore_api import *
 
 
-
 bot = telebot.TeleBot(TOKEN)
 
 
@@ -32,29 +31,81 @@ def send_help(message):
 
 
 
-def query_markup():
+def query_markup(response=None):
 	markup = InlineKeyboardMarkup()
-	markup.row_width = 3
-	markup.add(
+	'''if (response and response.ok):
+					temp = ""
+					for i in range(0, response.on_page):
+						if ((i + 1) % 5 == 0):
+							print(temp)
+							print('\n\n\n')
+							markup.row(temp)
+							temp = ""
+						temp = temp + "{'text': " + str(i) + ", 'callback_data': "+ str(i) +"}, "
+			
+				print(markup.row().to_dic())'''
+	markup.row(
+		InlineKeyboardButton("1", callback_data="1"),
+		InlineKeyboardButton("2", callback_data="2"),
+		InlineKeyboardButton("3", callback_data="3"),
+		InlineKeyboardButton("4", callback_data="4"),
+		InlineKeyboardButton("5", callback_data="5"))
+	markup.row(
+		InlineKeyboardButton("6", callback_data="6"),
+		InlineKeyboardButton("7", callback_data="7"),
+		InlineKeyboardButton("8", callback_data="8"),
+		InlineKeyboardButton("9", callback_data="9"),
+		InlineKeyboardButton("10", callback_data="10"))
+	markup.row(
 		InlineKeyboardButton("Prev", callback_data="prev_page"),
 		InlineKeyboardButton("Close", callback_data="close"),
 		InlineKeyboardButton("Next", callback_data="next_page"))
 	return markup
 
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
+	print(call.message.text)
+	if call.data == "1":
+		pass
+	if call.data == "2":
+		pass
+	if call.data == "3":
+		pass
+	if call.data == "4":
+		pass
+	if call.data == "5":
+		pass
+	if call.data == "6":
+		pass
+	if call.data == "7":
+		pass
+	if call.data == "8":
+		pass
+	if call.data == "9":
+		pass
+	if call.data == "10":
+		pass
+
 	if call.data == "close":
-		pass
-	if call.data == "prev_page":
-		pass
+		bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+
+	elif call.data == "prev_page":
+		bot.edit_message_text(text="previous", chat_id=call.message.chat.id,
+			message_id=call.message.message_id, inline_message_id=call.inline_message_id,
+			reply_markup=query_markup())
+
 	elif call.data == "next_page":
-		pass
+		bot.edit_message_text(text="next", chat_id=call.message.chat.id,
+			message_id=call.message.message_id, inline_message_id=call.inline_message_id,
+			reply_markup=query_markup())
+
 
 @bot.message_handler(commands=['books'])
 def list_books(message):
 	resp = endpoints.get.books(page=1)
 	resp_parsed = response_parsed(response=resp).books()
-	bot.send_message(message.chat.id, resp_parsed.reply, reply_markup=query_markup())
+	bot.send_message(message.chat.id, resp_parsed.reply, reply_markup=query_markup(resp_parsed))
 
 
 
